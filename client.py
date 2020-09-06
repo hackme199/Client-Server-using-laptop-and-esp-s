d41855110a55cc8079668a1f0dbdce8e601f3338
shikhar <hackme199@gmail.com>
2020-09-06 14:49:27 +0530

    altered f string version by custom function; fstrings are not supported in micropython

--- client.py
+++ client.py
@@ -1,12 +1,17 @@
-import usocket as socket
-import uselect as select
-import uerrno as errno
-import usys as sys
+import socket
+import select
+import errno
+import sys
 
 HEADER_LENGTH = 10
 
 IP = "192.168.43.165"
 PORT = 1234
+
+def f(a,b):
+    a = str(a)
+    return a + (b-len(a))*' '
+
 
 my_username = input("Username: ")
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,16 +20,18 @@ client_socket.connect((IP, PORT))
 client_socket.setblocking(False)
 
 username = my_username.encode("utf-8")
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+# username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+username_header = f(len(username),HEADER_LENGTH).encode('utf-8')
 client_socket.send(username_header + username)
 
 while 1:
-    message = input(f"{my_username} > ")
+    message = input("{} > ".format(my_username))
     # message = ''
 
     if message:
         message = message.encode("utf-8")
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        # message_header = f"{len(message):<{HEADER_LENGTH}}".encode("utf-8")
+        message_header = f(len(message),HEADER_LENGTH).encode('utf-8')
         client_socket.send(message_header + message)
     try:
         while 1:
@@ -40,14 +47,17 @@ while 1:
             message_length = int(message_header.decode("utf-8").strip())
             message = client_socket.recv(message_length).decode("utf-8")
 
-            print(f"{username} > {message}")
+            print("{} > {}".format(username,message))
 
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error',str(e))
-            sys.exit()
+    # except IOError as e:
+    #     if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+    #         print('Reading error',str(e))
+    #         sys.exit()
+    #     continue
+
+    # except Exception as e:
+    #     print('General error',str(e))
+    #     sys.exit()
+
+    except:
         continue
-
-    except Exception as e:
-        print('General error',str(e))
-        sys.exit()
